19d0ce5f5b30ee1e479c5d3cf81c9ccc3e09f41d
unknown <kaizukahayate@gmail.com>
2018-07-15 22:35:40 +0800

    Add React

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const axios = require('axios');
-const express = require('express');
+const express = require ('express');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const server = express();
 const path = require('path');
-const filemgr = require('./filemgr');
+// const filemgr = require('./filemgr');
+const Place = require('./Place');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
 
 server.use(bodyParser.urlencoded({extended:true}));
-
 server.set('view engine', 'hbs');
-
 hbs.registerPartials(__dirname + '/views/partials');
 
-const PLACES_API_KEY = 'AIzaSyB0L68gvLdQgJGMtJ6ALYoqV-brFoRUqS4'
+const PLACES_API_KEY = 'AIzaSyCOuQBxyC6T2uyyh5NDmTpvI5gk33ygk5c';
 var filteredResults;
 
-hbs.registerHelper('list',(items,options) => {
+hbs.registerHelper('list',(items,options)=>{
   items = filteredResults;
-  var out = "<tr><th>Name</th><th>Address</th><th>Photo</th></tr>";
-
+  var out ="<tr><th>Name</th>,<th>Address</th>,<th>Photos</th></tr>";
   const length = items.length;
 
-  for(var i=0; i<length; i++) {
+  for(var i=0;i<length;i++){
     out = out + options.fn(items[i]);
   }
   return out;
@@ -31,101 +30,107 @@ hbs.registerHelper('list',(items,options) => {
 
 server.use(express.static(path.join(__dirname,'images')));
 
-server.get('/',(req,res) => {
-  res.render('home.hbs');
-});
-
-server.get('/form',(req,res) => {
+server.get ('/form',(req,res)=>{
   res.render('form.hbs');
 });
 
-server.get('/historical',(req,res) => {
-  filemgr.getAllData().then((result) => {
-    filteredResults = result;
-  res.render('historical.hbs')
-
-}).catch((errorMessage) => {
-              console.log(errorMessage);
-
+server.get ('/',(req,res)=>{
+  res.render('home.hbs');
 });
-  });
 
 server.post('/getplaces',(req,res) => {
   const addr = req.body.address;
-  const placetype = req.body.placetype;
+  const placestype = req.body.placestype;
   const name = req.body.name;
+  const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyA9HmIKh3Yv-T9zS_JxjMZb8ZpEOpq7AtQ`;
 
-  const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyCXxsiK79-DXha-afMjHuLwohgNaSmRpXY`;
+  server.post('/delete', (req, res) => {
+  Place.remove({})
+    .then((result)=>{
+      res.status(200).send(result);
+    })
+    .catch((error)=>{
+      res.status(400).send(error);
+    })
+  })
 
 axios.get(locationReq).then((response) => {
   const locationData = {
-    addr: response.data.results[0].formatted_address,
-    lat: response.data.results[0].geometry.location.lat,
-    lng: response.data.results[0].geometry.location.lng,
+    addr:response.data.results[0].formatted_address,
+    lat:response.data.results[0].geometry.location.lat,
+    lng:response.data.results[0].geometry.location.lng,
   }
 
-  server.post('/delete', (req,res) => {
-      filemgr.deleteAll().then((result) => {
-        filteredResults = result;
-        res.render("historical.hbs");
-      }).catch((errorMessage) => {
-       console.log(errorMessage);
-     });
-    });
+const placesReq = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500&types=${placestype}&name=${name}&key=${PLACES_API_KEY}`;
 
-const placesReq = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=1500%20&types=${placetype}%20&name=${name}&key=${PLACES_API_KEY}`;
+return axios.get(placesReq);
 
- return axios.get(placesReq);
-}).then((response) => {
+}).then((response)=>{
 
-  filteredResults = extractData(response.data.results);
-
-  filemgr.saveData(filteredResults).then((result) =>{
-
-    //res.status(200).send(filteredResults);
-    res.render('results.hbs');
-
-  }).catch((erorMessage)=> {
-      console.log(errorMessage);
-  });
+filteredResults = extractData(response.data.results);
 
 
-}).catch((error) => {
+Place.insertMany(filteredResults)
+.then((result)=>{
+  res.status(200).send(result);
+})
+.catch((error)=>{
+  res.status(400).send(error);
+});
+
+
+}).catch((error)=>{
   console.log(error);
 });
 
+
 });
 
-const extractData = (originalResults) => {
+server.post('/historical',(req,res)=>{
+Place.find({})
+  .then((result)=> {
+    res.status(200).send(results);
+  })
+  .catch((error)=>{
+    res.status(400).send(error);
+  })
+
+});
+
+const extractData = (originalResults)=>{
   var placesObj = {
-    table: [],
+    table:[],
   };
 
   const length = originalResults.length;
+  for (var i=0; i<length; i ++){
+if(originalResults[i].photos){
+  const photoRef = originalResults[i].photos[0].photo_reference;
+  const requestUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${PLACES_API_KEY}`
+  tempObj={
+    name: originalResults[i].name,
+    address:originalResults[i].vicinity,
+    photo_reference:requestUrl,
 
-  for (var i=0; i<length; i++) {
-    if(originalResults[i].photos){
-      const photoRef = originalResults[i].photos[0].photo_reference;
-      const requestUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${PLACES_API_KEY}`
 
-      tempObj = {
-        name:originalResults[i].name,
-        address: originalResults[i].vicinity,
-        photo_reference: requestUrl,
-      }
-    }  else {
-      tempObj = {
-        name:originalResults[i].name,
-        address: originalResults[i].vicinity,
-        photo_reference: '/noimage.png',
-      }
-    }
-    placesObj.table.push(tempObj);
+  }
+}else{
+  tempObj={
+    name: originalResults[i].name,
+    address:originalResults[i].vicinity,
+    photo_reference:'/no_image_found.png',
+
+
   }
 
+}
+
+
+  placesObj.table.push(tempObj);
+  }
   return placesObj.table;
 };
 
-server.listen(port,()=> {
+server.listen(port,()=>{
   console.log(`Server started on port ${port}`);
 });
--- filemgr.js
+++ filemgr.js
@@ -1,55 +1,52 @@
 const {MongoClient} = require('mongodb');
 const fs = MongoClient;
 
-const database = 'mongodb://localhost:27017';
-//const database ='mongodb://places:realmadrid03@ds055495.mlab.com:55495/placesapp';
+ const database = 'mongodb://localhost:27017';
+// const database = 'mongodb://places:realmadrid03@ds055495.mlab.com:55495/placesapp';
 
-const appname ='placesapp';
-const collectionname ='placesappcollection';
+const appname = 'placesapp';
+const collectionname ='placesappcollection'
 
-const saveData = (newdata) =>  {
-  return new Promise((resolve,reject) => {
-    MongoClient.connect(database,{useNewUrlParser:true},(err,client) => {
-      if (err) {
-          reject('Unable to connect to MongoDB');
-
-      }
-
-      console.log('Connected to MongoDB in saveData');
-      const db = client.db(appname);
-
-      const length = newdata.length;
-      for(var i=0; i<length; i++) {
-
-      db.collection(collectionname).insertOne(newdata[i],
-        (err,result) => {
-        if(err) {
-          reject('Unable to insert');
-        }
-
-      });
-    }
-        resolve(1);
-      client.close();
-    });
-  });
-};
-
-const getAllData = ()  => {
-  return new Promise((resolve,reject) => {
-    MongoClient.connect(database,{useNewUrlParser:true},(err,client) => {
+const saveData = (newdata) => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
       if (err) {
         reject('Unable to connect to MongoDB');
-
       }
 
       console.log('Connected to MongoDB');
       const db = client.db(appname);
 
+      const length = newdata.length;
+      for(var i=0; i<length; i++){
+
+      db.collection(collectionname).insertOne(newdata[i], (err, result) => {
+        if (err) {
+          reject('Unable to insert');
+        }
+
+      });
+}
+resolve(1);
+
+      client.close();
+    });
+  });
+};
+
+const getAllData = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
+      if (err) {
+        reject('Unable to connect to MongoDB');
+      }
+
+      console.log('Connected to MongoDB');
+      const db = client.db(appname);
 
       db.collection(collectionname).find().toArray().then( (docs) => {
         resolve(docs);
-      },(err) => {
+      }, (err) => {
         reject('Unable to fetch docs');
       });
 
@@ -58,12 +55,11 @@ const getAllData = ()  => {
   });
 };
 
-const deleteAll = ()  => {
-  return new Promise((resolve,reject) => {
-    MongoClient.connect(database,{useNewUrlParser:true},(err,client) => {
+const deleteAll = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
       if (err) {
         reject('Unable to connect to MongoDB');
-
       }
 
       console.log('Connected to MongoDB');
@@ -71,7 +67,7 @@ const deleteAll = ()  => {
 
       db.collection(collectionname).remove({}).then( (result) => {
         resolve(result);
-      },(err) => {
+      }, (err) => {
         reject('Unable to delete');
       });
 
@@ -79,6 +75,9 @@ const deleteAll = ()  => {
     });
   });
 };
+
+
+
 
 module.exports = {
   saveData,
