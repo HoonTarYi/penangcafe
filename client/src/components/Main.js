ebafcb9bcfd8f5fbb16f29c4dab3a9b732975e38
unknown <kaizukahayate@gmail.com>
2018-07-16 20:09:08 +0800

    Main.Js Updated

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 class Main extends Component {
   render() {
     return (
+      <html>
+      <title>Search Nearby Cafe</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+
+      <style>
+      body, html {
+          height: 100%;
+          font-family: "Inconsolata", sans-serif;
+      }
+      .bgimg {
+          background-position: center;
+          background-size: cover;
+          background-image: url('header.jpg');
+          min-height: 75%;
+      }
+      .menu {
+          display: none;
+      }
+      </style>
+      <body>
       <!-- Header with image -->
       <header class="bgimg w3-display-container w3-grayscale-min" id="home">
         <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
@@ -87,7 +110,8 @@ class Main extends Component {
         </div>
       </div>
 
-
+      </body>
+      </html>
 
 
       <!-- Footer -->
