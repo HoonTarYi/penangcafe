bb710a4f2319f540a0d19357a923c68b695e227b
unknown <kaizukahayate@gmail.com>
2018-07-16 20:56:30 +0800

    Main.Js Updated

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -16,22 +16,23 @@ class Main extends Component {
         </div>
       </header>
 
-      <div className="w3-sand w3-grayscale w3-large"></div>
+    <div>
+      <div className="w3-sand w3-grayscale w3-large">
 
-        <div className="w3-content" style="max-width:700px">
+          <div className="w3-content" style="max-width:700px">
           <h5 className="w3-center w3-padding-64"><span className="w3-tag w3-wide">ABOUT THE CAFE</span></h5>
           <p>The Cafe was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <p>In addition to our full espresso and brew bar menu, we serve fresh made-to-order breakfast and lunch sandwiches, as well as a selection of sides and salads and other good stuff.</p>
-          <div className="w3-panel w3-leftbar w3-light-grey">
-            <p><i>"Use products from nature for what it's worth - but never too early, nor too late." Fresh is the new sweet.</i></p>
-            <p>Chef, Coffeeist and Owner: Liam Brown</p>
-          </div>
+            <div className="w3-panel w3-leftbar w3-light-grey">
+              <p><i>"Use products from nature for what it's worth - but never too early, nor too late." Fresh is the new sweet.</i></p>
+              <p>Chef, Coffeeist and Owner: Liam Brown</p>
+            </div>
           <img src="cafe.jpg" style="width:100%;max-width:1000px" className="w3-margin-top" alt="img">
           <p><strong>Opening hours:</strong></p>
           <p><strong>Address:</strong> 15 Adr street, 5015, NY</p>
         </div>
-        </div>
-
+          </div>
+              </div>
 
 
       <div className="w3-container" id="menu">
