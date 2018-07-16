8a375c68e103b3fa2b955a651711bd7db3a97339
unknown <kaizukahayate@gmail.com>
2018-07-16 20:32:36 +0800

    Main.Js Updated

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,7 +9,7 @@ class Main extends Component {
           <span className="w3-tag">24 Hours</span>
         </div>
         <div className="w3-display-middle w3-center">
-          <span className="w3-text-white" style="font-size:90px">the<br>Cafe</span>
+          <span className="w3-text-white" style="font-size:90px">the Cafe</span>
         </div>
         <div className="w3-display-bottomright w3-center w3-padding-large">
           <span className="w3-text-white">Find Your Desire Cafe</span>
@@ -52,16 +52,16 @@ class Main extends Component {
 
           <div id="Eat" className="w3-container menu w3-padding-48 w3-card">
             <h5>Bread Basket</h5>
-            <p className="w3-text-grey">Assortment of fresh baked fruit breads and muffins</p><br>
+            <p className="w3-text-grey">Assortment of fresh baked fruit breads and muffins</p><
 
             <h5>Honey Almond Granola with Fruits</h5>
-            <p className="w3-text-grey">Natural cereal of honey toasted oats, raisins, almonds and dates</p><br>
+            <p className="w3-text-grey">Natural cereal of honey toasted oats, raisins, almonds and dates</p>
 
             <h5>Belgian Waffle</h5>
-            <p className="w3-text-grey">Vanilla flavored batter with malted flour</p><br>
+            <p className="w3-text-grey">Vanilla flavored batter with malted flour</p>
 
             <h5>Scrambled eggs</h5>
-            <p className="w3-text-grey">Scrambled eggs, roasted red pepper and garlic, with green onions</p><br>
+            <p className="w3-text-grey">Scrambled eggs, roasted red pepper and garlic, with green onions</p>
 
             <h5>Blueberry Pancakes</h5>
             <p className="w3-text-grey">With syrup, butter and lots of berries</p>
@@ -69,16 +69,16 @@ class Main extends Component {
 
           <div id="Drinks" className="w3-container menu w3-padding-48 w3-card">
             <h5>Americano</h5>
-            <p className="w3-text-grey">A shot or two of espresso with hot water added</p><br>
+            <p className="w3-text-grey">A shot or two of espresso with hot water added</p>
 
             <h5>Cappuccino</h5>
-            <p className="w3-text-grey">Espresso with a little bit of steamed milk topped with a lot of foam</p><br>
+            <p className="w3-text-grey">Espresso with a little bit of steamed milk topped with a lot of foam</p>
 
             <h5>Mocha</h5>
-            <p className="w3-text-grey">Espresso with steamed milk and chocolate (you can add whipped cream as well!)</p><br>
+            <p className="w3-text-grey">Espresso with steamed milk and chocolate (you can add whipped cream as well!)</p>
 
             <h5>>Espresso</h5>
-            <p className="w3-text-grey">Coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground beans.</p><br>
+            <p className="w3-text-grey">Coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground beans.</p>
 
             <h5>Soda</h5>
             <p className="w3-text-grey">Coke, Sprite, Fanta, etc.</p>
