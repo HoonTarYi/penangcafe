80d0197cf65becd5ba0ecc7cf1686066ad7b9131
unknown <kaizukahayate@gmail.com>
2018-07-16 16:44:36 +0800

    Change navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,21 +4,19 @@ import {Link} from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <div class="w3-top">
-        <div class="w3-row w3-padding w3-black">
-          <div class="w3-col s3">
-            <a href="/" class="w3-button w3-block w3-black">HOME</Link></a>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link to="#" className="navbar-brand">Places</Link>
+            </div>
+            <ul className="nav navbar-nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="form">Form</Link></li>
+              <li><Link to="historical">Previous Searches</Link></li>
+            </ul>
           </div>
-          <div class="w3-col s3">
-            <a Link to="#about" class="w3-button w3-block w3-black">ABOUT</Link></a>
-          </div>
-          <div class="w3-col s3">
-            <a Link to="form" class="w3-button w3-block w3-black">Form</Link></a>
-          </div>
-          <div class="w3-col s3">
-            <a Link to="historical" class="w3-button w3-block w3-black">Previous Searches</Link></a>
-          </div>
-        </div>
+        </nav>
       </div>
 
     );
