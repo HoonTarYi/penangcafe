f8e942e08a98978234e11706172402d982604599
unknown <kaizukahayate@gmail.com>
2018-07-17 21:16:31 +0800

    changed no image path

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ const extractData = (originalResults) => {
       tempObj = {
         name: originalResults[i].name,
         address: originalResults[i].vicinity,
-        photo_reference: '/no-image.jpg',
+        photo_reference: '/{noimage}',
       }
     }
 
