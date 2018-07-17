f5f9d95b41a47d96c89898baec9f60f43c292c1e
unknown <kaizukahayate@gmail.com>
2018-07-17 22:00:29 +0800

    Mainjs

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -12,27 +12,23 @@ class Main extends Component {
 
       <div>
 
-        <div className="bgimg w3-display-container w3-grayscale-min" id="home">
-          <div className="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
-            <span className="w3-tag">24 Hours</span>
-              </div>
-              <div className="w3-display-middle w3-center">
-
-                  <img src={header}  className="w3-margin-top" alt="img" min-height="75%" />
-                <span className="w3-text-white">the Cafe</span>
-
-                </div>
-                  <div className="w3-display-bottomright w3-center w3-padding-large">
-                    <span className="w3-text-white">Find Your Desire Cafe</span>
-                    </div>
-                        </div>
-
-
-
-
-
       <div className="w3-sand w3-grayscale w3-large">
 
+      <div className="bgimg w3-display-container w3-grayscale-min" id="home">
+        <div className="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
+          <span className="w3-tag">24 Hours</span>
+            </div>
+            <div className="w3-display-middle w3-center">
+
+                <img src={header}  className="w3-margin-top" alt="img" min-height="75%" />
+              <span className="w3-text-white">the Cafe</span>
+
+              </div>
+                <div className="w3-display-bottomright w3-center w3-padding-large">
+                  <span className="w3-text-white">Find Your Desire Cafe</span>
+                  </div>
+
+                      </div>
           <div className="w3-content">
           <h5 className="w3-center w3-padding-64"><span className="w3-tag w3-wide">ABOUT THE CAFE</span></h5>
           <p>This website is to let user find the nearby cafe. User just need to insert the address , enter the cafe name and you can start search the nearby cafe of the address that had enter.</p>
